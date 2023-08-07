664451110b6fb2f6f08ee87b88272dffa5a92882
Ju Gabriella Sá <111814472+JulieGab@users.noreply.github.com>
2023-08-07 12:01:45 -0300

    Initial commit

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import './Main.css'
+
+// icones do formulário
+import {FaPlus} from 'react-icons/fa'
+
+export default class Main extends Component{ //Component é o nome de uma classe herança, que já vem 
+
+    state = {
+                novaTarefa: '',
+                tarefas:[
+                    "Tomar banho",
+                    "Café da manhã",
+                    "Estudar"
+                ]
+            };
+
+    mudaInput = (e) =>{
+        this.setState({
+            novaTarefa: e.target.value
+        })
+    }
+
+    render(){
+        const {novaTarefa, tarefas} = this.state
+        return (
+            <div className="main">
+                <h1>Lista de Tarefas</h1>
+                <form action="#" className="form">
+                    <input onChange={this.mudaInput} type="text"></input>
+                    <button type="submit"><FaPlus/></button>
+                </form>
+                <ul className="listaTarefas">
+                    {tarefas.map(tarefa =>{
+                       return <li key={tarefa}>
+                            {tarefa}
+                        </li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
